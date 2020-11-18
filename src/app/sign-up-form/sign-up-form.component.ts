6ddb681a86de9ae12ad7f1054cdba9274134b3e3
Colinpp1 <colinpp1@gmail.Com>
2020-11-18 15:37:07 +0200

    Validation_complete

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -9,19 +9,52 @@ import {SignUp} from '../models/sign-up.model';
 })
 export class SignUpFormComponent implements OnInit {
 
-  model = new SignUp('Test','Test','Test','Test');
-  submitted  =false;
+  model = new SignUp('','','','');
   regexp;
-  onSubmit() {  }
+  validate =false;
 
-  newSignUp() {
-    this.model = new SignUp("","","","");
+  OnSubmit(){
+
   }
-
   validateEmail(){
+    if (this.validate ==false)
+    {
+      return true;
+    }
     this.regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     return this.regexp.test(this.model.email);
   }
+  
+  validateName(){
+    if(this.model.first_name !='' || this.validate==false)
+    {
+     // this.model.first_name.
+       return true;
+    }
+    return false;
+
+  }
+  validateSurname(){
+    if(this.model.last_name !='' || this.validate==false)
+    {
+       return true;
+    }
+    return false;
+
+  }
+
+  validatePassword(){
+    if(this.model.password !='' || this.validate==false)
+    {
+       return true;
+    }
+    return false;
+
+  }
+  Validation(){
+    this.validate =true;
+  }
+
   get diagnostic() { return JSON.stringify(this.model); }
 
   showFormControls(form:any){
